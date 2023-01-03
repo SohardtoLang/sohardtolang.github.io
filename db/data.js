5a6eea0bf77d077b905fccdfee4cbd0ae66b1e6f
SohardtoLang <119798959+SohardtoLang@users.noreply.github.com>
2023-01-03 12:22:14 +0700

    Add files via upload

---
+++ db/data.js
@@ -0,0 +1,68 @@
+var arrayApk = [
+  {
+    "image":"https://telegra.ph/file/4a4d02e2ea50da13c7e1c.png",
+    "title":"WhatsApp GB - Mod Apk",
+    "url":"https://www.gbwhatsapp.download/gbwhatsapp.html?filename=gwggad&from=gwggad&gclid=EAIaIQobChMIr_ecs-ao_AIVOpVLBR0GyAkKEAAYASAAEgIT7_D_BwE#/mobile"
+  },
+  {
+    "image":"https://apkmodget.com/media/2022/05/_2/180x180/free-fire-mod-apk_0000f.jpg",
+    "title":"Free Fire Biasa - Mod Apk",
+    "url":"https://apkmodget.com/id/permainan/free-fire-mod-apk-unlimited-diamonds-download-1/"
+  },
+  {
+    "image":"https://apkmodget.com/media/2022/12/_2/180x180/mobile-legends-apk_410b1.jpg",
+    "title":"Mobile Legends - Mod Apk",
+    "url":"https://apkmodget.com/games/mobile-legends-mod-apk-download/"
+  },
+  {
+    "image":"https://apkmodget.com/media/2022/09/_1/80x80/capture_db08c.jpg",
+    "title":"Telegram Mod - Mod Apk",
+    "url":"https://apkmodget.com/apps/telegram-apk-download/"
+  },
+  {
+    "image":"https://apkmodget.com/media/2022/09/_2/180x180/capture_849df.jpg",
+    "title":"Pubg Lite - Mod Apk",
+    "url":"https://apkmodget.com/games/pubg-mobile-lite-apk/"
+  },
+  {
+    "image":"https://apkmodget.com/media/2022/12/_2/180x180/instagram-apk_82f87.jpg",
+    "title":"Instagram - Mod Apk",
+    "url":"https://apkmodget.com/apps/instagram-apk/"
+  },
+    {
+    "image":"https://apkmodget.com/media/2022/12/_2/180x180/tiktok-premium-apk_af9fd.jpg",
+    "title":"Tiktok - Mod Apk",
+    "url":"https://apkmodget.com/apps/tiktok-premium-apk/"
+  },
+  {
+    "image":"https://apkmodget.com/media/2022/12/_2/180x180/tiktok-lite-apk_47c40.jpg",
+    "title":"Tiktok Lite - Mod Apk",
+    "url":"https://apkmodget.com/media/2022/12/_2/180x180/tiktok-lite-apk_47c40.jpg"
+  },
+  {
+    "image":"https://apkmodget.com/media/2022/12/_2/180x180/snack-video-apk_5c9ee.jpg",
+    "title":"Snack Video - Mod Apk",
+    "url":"https://apkmodget.com/apps/snack-video-apk/"
+  },
+  {
+    "image":"https://apkmodget.com/media/2022/12/_2/180x180/facebook-pro-apk_c4585.jpg",
+    "title":"Facebook - Mod Apk",
+    "url":"https://apkmodget.com/apps/facebook-pro-apk/"
+  },
+  {
+    "image":"https://apkmodget.com/media/2022/12/_2/180x180/whatsapp-business-apk_86f0f.jpg",
+    "title":"WhatsApp Business - Mod Apk",
+    "url":"https://apkmodget.com/apps/whatsapp-business-apk/"
+  },
+  {
+    "image":"https://apkmodget.com/media/2022/12/_2/180x180/bigo-live-apk_54326.jpg",
+    "title":"Bigo Live - Mod Apk",
+    "url":"https://apkmodget.com/apps/bigo-live-apk-download/"
+  },
+]
+
+// {
+//    "image":"Image apk",
+//    "title":"Nama apk",
+//    "url":"url apk"
+//  },
